7b92e1382ae7714104b0cd6d0635b1121be90143
kellytapuelpan <kellytapuelpan@gmail.com>
2020-02-10 16:04:27 -0300

    Cambio en js para que aparezcan según orden de id

--- api01_pokemon/src/js/functions.js
+++ api01_pokemon/src/js/functions.js
@@ -6,7 +6,28 @@ $( document ).ready(function(pokemon) {
 	pokemon = range(1, 152);
 	for(i=1; i<pokemon.length; i++) {
 		$.get( "https://pokeapi.co/api/v2/pokemon/" + i, function( data ) {
-		  	$( ".row" ).append("<img src=" + data.sprites.front_default + " alt=" + data.name +">" );
+			var $newDiv = $("<div id="+ data.id + "><img id=" + data.id + " src=" + data.sprites.front_default + " alt=" + data.name +"></div>");
+			
+			if(data.id < 10){
+				$newDiv.attr("id", "pokemon0" + data.id);
+				$newDiv.attr("data-id", "0" + data.id);
+			} else {
+				$newDiv.attr("id", "pokemon" + data.id);
+				$newDiv.attr("data-id", data.id);
+			}
+
+		  	$( ".row" ).append($newDiv);
+
+		  	var mypokemonlist = $('.row');
+			var listitems = mypokemonlist.children('div').get();
+			listitems.sort(function(a, b) {
+			    var compA = $(a).attr('id').toUpperCase();
+			    var compB = $(b).attr('id').toUpperCase();
+			    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+			})
+			$.each(listitems, function(idx, itm) {
+			    mypokemonlist.append(itm);
+			});
 		}, "json" );
 	}
 });
