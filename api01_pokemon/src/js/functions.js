d0d2dc483ea1430d9079888a55d41ce7c33a8f12
kellytapuelpan <kellytapuelpan@gmail.com>
2020-02-27 17:01:21 -0300

    Fix primary function

--- api01_pokemon/src/js/functions.js
+++ api01_pokemon/src/js/functions.js
@@ -1,35 +1,27 @@
-$( document ).ready(function(pokemon) {
+$(document).ready(function(){
+
 	function range(start, end) {
   		return Array(end - start + 1).fill().map((_, idx) => start + idx);
 	}
 
-	pokemon = range(1, 152);
-	for(i=1; i<pokemon.length; i++) {
-		$.get( "https://pokeapi.co/api/v2/pokemon/" + i, function( data ) {
-			var $newDiv = $("<div id="+ data.id + "><img id=" + data.id + " src=" + data.sprites.front_default + " alt=" + data.name +"></div>");
-			
-			if(data.id < 10){
-				$newDiv.attr("id", "pokemon-a" + data.id);
-				$newDiv.attr("data-id", "0" + data.id);
-			} else if(data.id < 100) {
-				$newDiv.attr("id", "pokemon-b" + data.id);
-			} else {
-				$newDiv.attr("id", "pokemon" + data.id);
-				$newDiv.attr("data-id", data.id);
-			}
+	pokemonrange = range(1, 151);
+	poke_id = pokemonrange;
 
-		  	$( ".row" ).append($newDiv);
+	$.each(poke_id, function(i, value) {
+		url = 'https://pokeapi.co/api/v2/pokemon/' + poke_id[i];
+	    $.get(url, function(res) {
+	        var html_str = '';
+	        html_str += '<img id="' + res.id + '" src=' + res.sprites.front_default + ' alt=' + res.name + ' class="img-poke">';
 
-		  	var mypokemonlist = $('.row');
-			var listitems = mypokemonlist.children('div').get();
-			listitems.sort(function(a, b) {
-			    var compA = $(a).attr('id').toUpperCase();
-			    var compB = $(b).attr('id').toUpperCase();
-			    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-			})
-			$.each(listitems, function(idx, itm) {
-			    mypokemonlist.append(itm);
+	        $('.col-pokemon').append(html_str);
+
+	        $('.col-pokemon img').sort(function(a, b) {
+			  	return parseInt(a.id) - parseInt(b.id);
+			}).each(function() {
+			  	var elem = $(this);
+			  	elem.remove();
+			  	$(elem).appendTo('.col-pokemon');
 			});
-		}, "json" );
-	}
+	    }, "json");
+	});
 });
