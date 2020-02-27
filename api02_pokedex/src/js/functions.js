d0b4816c11ea73aa8e2f4354e820e7d7404de050
kellytapuelpan <kellytapuelpan@gmail.com>
2020-02-27 16:51:12 -0300

    Simplify function

--- api02_pokedex/src/js/functions.js
+++ api02_pokedex/src/js/functions.js
@@ -1,50 +1,50 @@
-$( document ).ready(function() {
+$(document).ready(function(){
 
 	function range(start, end) {
   		return Array(end - start + 1).fill().map((_, idx) => start + idx);
 	}
 
-	pokemonrange = range(1, 152);
+	pokemonrange = range(1, 151);
+	poke_id = pokemonrange;
 
-	for(i=1; i<pokemonrange.length; i++) {
-		url = "https://pokeapi.co/api/v2/pokemon/" + i;	
-		$.get( url, function( data ) {
+	$.each(poke_id, function(i, value) {
+		url = 'https://pokeapi.co/api/v2/pokemon/' + poke_id[i];
+	    $.get(url, function(res) {
+	        var html_str = '';
+	        html_str += '<div id="' + res.id + '" class="cont-poke">';
+	        	html_str += '<div class="cont-img" data-toggle="modal" data-id=' + res.id + ' data-target="#div' + res.id + '">';
+	        		html_str += '<img id="img' + res.id + '" src=' + res.sprites.front_default + ' alt=' + res.name + ' class="img-poke">';
+	        	html_str += '</div>';
+	        	html_str += '<div class="data-poke modal fade" id="div' + res.id + '" tabindex="-1" role="dialog" aria-labelledby="div' + res.id + 'Label" aria-hidden="true">';
+			        html_str += '<div class="modal-dialog text-center">';
+				        html_str += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
+							html_str += '<span aria-hidden="true">x</span>';
+						html_str += '</button>';
+						html_str += '<h4 class="pt-4">'+ res.name +'</h4>';
+						html_str += '<img id="alt"' + res.id + '" src=' + res.sprites.front_shiny + ' alt=' + res.name + ' class="img-poke-modal">';
+				        html_str += '<div class="info-poke pb-3">';
+					        html_str += '<p class="mb-1"><strong>Types:</strong></p>';
+					        html_str += '<ul class="mb-2">'; 
+					        for(var i = 0; i < res.types.length; i++) {
+					            html_str += '<li>- ' + res.types[i].type.name + '</li>';
+					        }
+					        html_str += '</ul>';
+					        html_str += '<p class="mb-1"><strong>Height:</strong> ' + res.height + '</p>';
+					        html_str += '<p class="mb-1"><strong>Weight:</strong> ' + res.weight + '</p>';
+					    html_str += '</div>';
+				    html_str += '</div>';
+			    html_str += '</div>';
+	        html_str += '</div>';
 
-			var obj = data.types;
+	        $('.col-pokemon').append(html_str);
 
-			for(j=0; j<obj.length; j++) {
-				if(j == 0) {
-					listTypes = data.types[0].type.name;
-				} else if(j == 1) {
-					listTypes = data.types[0].type.name + ', ' +data.types[1].type.name;
-				}
-			}
-
-			var $newDiv = $("<div class='cont-poke'><div class='cont-img' data-toggle='modal' data-id=" + data.id + " data-target='#div" + data.id + "'><img id=" + data.id + " src=" + data.sprites.front_default + " alt=" + data.name +" class='img-poke'></div><div class='data-poke modal fade' id='div" + data.id + "' tabindex='-1' role='dialog' aria-labelledby='div" + data.id + "Label' aria-hidden='true'><div class='modal-dialog text-center'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>x</span></button><h4 class='pt-4'>"+ data.name +"</h4><img id='alt" + data.id + "' src=" + data.sprites.front_shiny + " alt="+ data.name +" class='img-poke-modal'><div class='info-poke pb-3'><ul><li><strong>Types: </strong>"+ listTypes +"</li><li><strong>Height:</strong> " + data.height + "</li><li><strong>Weight:</strong> " + data.weight + "</li></ul></div></div></div></div>");
-			if(data.id < 10){
-				$newDiv.attr("id", "pokemon-a" + data.id);
-				$newDiv.attr("data-id", "0" + data.id);
-			} else if(data.id <= 99) {
-				$newDiv.attr("id", "pokemon-b" + data.id);
-				$newDiv.attr("data-id", data.id);
-			} else {
-				$newDiv.attr("id", "pokemon" + data.id);
-				$newDiv.attr("data-id", data.id);
-			}
-
-			$(".col-pokemon").append($newDiv);
-		   
-			var mylist = $('.col-pokemon');
-			var listitems = mylist.children('div.cont-poke').get();
-			listitems.sort(function(a, b) {
-			    var compA = $(a).attr('id').toUpperCase();
-			    var compB = $(b).attr('id').toUpperCase();
-			    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-			})
-			$.each(listitems, function(idx, itm) {
-			    mylist.append(itm);
+	        $('.col-pokemon .cont-poke').sort(function(a, b) {
+			  	return parseInt(a.id) - parseInt(b.id);
+			}).each(function() {
+			  	var elem = $(this);
+			  	elem.remove();
+			  	$(elem).appendTo('.col-pokemon');
 			});
-
-		}, "json" );
-	}
+	    }, "json");
+	});
 });
