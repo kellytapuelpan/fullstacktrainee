2dec61b932b888d459cc261ee99dce7cb78978a3
kellytapuelpan <kellytapuelpan@gmail.com>
2020-04-13 19:32:31 -0400

    Add files via upload

---
+++ javaFun/MyName.java
@@ -0,0 +1,7 @@
+public class MyName {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Kelly Puelpan");
+        System.out.println("Tengo xx años de edad");
+        System.out.println("Vivo en Viña del Mar");
+    }
+}
