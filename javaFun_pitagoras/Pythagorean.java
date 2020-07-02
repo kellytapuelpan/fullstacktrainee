7f50fffeaee8d806a517b263d88f927052f90e75
kellytapuelpan <kellytapuelpan@gmail.com>
2020-07-02 10:40:08 -0400

    Add files via upload

--- javaFun_pitagoras/Pythagorean.java
+++ javaFun_pitagoras/Pythagorean.java
@@ -1,7 +1,15 @@
 import java.util.*;
 public class Pythagorean {
     public double calculateHypotenuse(int legA, int legB) {
-    	Double legC = Math.sqrt(Math.pow(legA,3) + Math.pow(legB,1));
+    	Scanner cat1 = new Scanner(System.in);
+        System.out.println("Ingresa el valor del primer cateto");
+		legA = cat1.nextInt();
+
+		Scanner cat2 = new Scanner(System.in);
+        System.out.println("Ingresa el valor del segundo cateto");
+		legB = cat2.nextInt();
+		
+    	double legC = Math.sqrt(Math.pow(legA,2) + Math.pow(legB,2));
 		return legC;
     }
 }
--- javaFun_pitagoras/PythagoreanTest.java
+++ javaFun_pitagoras/PythagoreanTest.java
@@ -1,7 +1,13 @@
+import java.util.*;
 public class PythagoreanTest {
     public static void main(String[] args) {
+    	int legA = 0;
+    	int legB = 0;
+    	double legC = 0;
+    	
         Pythagorean p = new Pythagorean();
-        Double getHypo = p.calculateHypotenuse(legA,legB);
+
+        double getHypo = p.calculateHypotenuse(legA,legB);
         System.out.println("El valor de la Hipotenusa es " + getHypo);
     }
 }
